56d277f2c7884346fc0acf5ad69da69e6c98c943
dalke <dalke@localhost>
2010-02-15 04:48:07 +0100

    Decided I wanted to promote these as top-level functions.

--- chemfp/__init__.py
+++ chemfp/__init__.py
@@ -23,3 +23,52 @@
 # ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 # CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
+
+
+def open(filename):
+    ext = filename[-4:].lower()
+    if ext == ".fps":
+        from chemfp import fps_reader
+        return fps_reader.open(filename)
+    elif ext == ".fpb":
+        raise NotImplementedError("No support yet for .fpb files")
+    # Should I open and sniff?
+    raise NotImplementedError("Unknown fingerprint format")
+
+def tanimoto_count(query, targets, threshold=0.0):
+    if not (0.0 <= threshold <= 1.0):
+        raise TypeError("threshold must be between 0.0 and 1.0, inclusive")
+
+    f = getattr(targets, "_chemfp_tanimoto_count", None)
+    if f is not None:
+        return f(query, threshold)
+    
+    from chemfp import search
+    return search.generic_tanimoto_count(query, targets, k, threshold)
+
+
+def tanimoto(query, targets, threshold=0.0):
+    if not (0.0 <= threshold <= 1.0):
+        raise TypeError("threshold must be between 0.0 and 1.0, inclusive")
+
+    f = getattr(targets, "_chemfp_tanimoto", None)
+    if f is not None:
+        return f(query, threshold)
+    
+    from chemfp import search
+    return search.generic_tanimoto(query, targets, k, threshold)
+
+def tanimoto_knearest(query, targets, k, threshold=0.0):
+    if not (k > 0):
+        raise TypeError("k must be a postive integer")
+    if not (0.0 <= threshold <= 1.0):
+        raise TypeError("threshold must be between 0.0 and 1.0, inclusive")
+
+    # Allow the targets to override the default search code
+    f = getattr(targets, "_chemfp_tanimoto_knearest", None)
+    if f is not None:
+        return f(query, k, threshold)
+
+    from chemfp import search
+    return search.generic_tanimoto_knearest(query, targets, k, threshold)
+
