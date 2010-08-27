b421d1b666e2da10a619eeae6c3269361750b00d
dalke <dalke@localhost>
2010-08-27 18:20:48 -0400

    Moved the chemfp_strerror function

--- src/fps.c
+++ src/fps.c
@@ -7,24 +7,6 @@
 #include "chemfp.h"
 
 
-const char *chemfp_strerror(int err) {
-  switch (err) {
-  case CHEMFP_OK: return "Ok";
-  case CHEMFP_BAD_ARG: return "Bad argument";
-
-  case CHEMFP_UNSUPPORTED_WHITESPACE: return "Unsupported whitespace";
-  case CHEMFP_MISSING_FINGERPRINT: return "Missing fingerprint field";
-  case CHEMFP_BAD_FINGERPRINT: return "Fingerprint field is in the wrong format";
-  case CHEMFP_UNEXPECTED_FINGERPRINT_LENGTH: return "Fingerprint is not the expected length";
-  case CHEMFP_MISSING_ID: return "Missing id field";
-  case CHEMFP_BAD_ID: return "Id field is in the wrong format";
-  case CHEMFP_MISSING_NEWLINE: return "Line must end with a newline character";
-
-  default: return "Unknown error";
-  }
-}
-	
-
 /* Use <= to preserve order */
 /* Python changed from <= to < . See comments in heapq.c */
 /* Should I use the unique index as the secondary key? */
