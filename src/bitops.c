3ab6c36c20fb126108ea5d2326f52a650f124414
dalke <dalke@localhost>
2010-10-12 18:53:31 -0400

    comment cleanups, and got rid of tabs

--- src/bitops.c
+++ src/bitops.c
@@ -11,7 +11,7 @@
   A hex fingerprint is also stored as a length and a sequence of bytes
   but each byte encode 4 bits of the fingerprint as a hex character. The
   only valid byte values are 0-9, A-F and a-f. Other values will cause
-  an error value to be returned.
+  an error value to be returned. */
 
 /***** Functions for hex fingerprints ******/
 
@@ -86,7 +86,7 @@ int chemfp_hex_isvalid(int len, const unsigned char *fp) {
   /* Out of range values set 0x10 so do cumulative bitwise-or and see if that
      bit is set. Optimize for the expected common case of validfingerprints. */
   for (i=0; i<len; i++) {
-	union_w |= hex_to_value[fp[i]];
+    union_w |= hex_to_value[fp[i]];
   }
   return (union_w < BIG) ? 1 : 0;
 }
@@ -97,8 +97,8 @@ int chemfp_hex_popcount(int len, const unsigned char *fp) {
 
   for (i=0; i<len; i++) {
     /* Keep track of the cumulative popcount and the cumulative bitwise-or */
-	popcount += hex_to_popcount[fp[i]];
-	union_w |= hex_to_value[fp[i]];
+    popcount += hex_to_popcount[fp[i]];
+    union_w |= hex_to_value[fp[i]];
   }
   if (union_w >= BIG) {
     return -1;  /* Then this was an invalid fingerprint (contained non-hex characters) */
@@ -109,30 +109,30 @@ int chemfp_hex_popcount(int len, const unsigned char *fp) {
 /* Return the population count of the intersection of two hex fingerprints,
    otherwise return -1. */
 int chemfp_hex_intersect_popcount(int len, const unsigned char *fp1,
-								  const unsigned char *fp2) {
+                                  const unsigned char *fp2) {
   int i, union_w=0, intersect_popcount=0;
   int w1, w2;
 
   for (i=0; i<len; i++) {
     /* Get the popcount for each hex value. (Or 0 for non-hex values.) */
-	w1 = hex_to_value[fp1[i]];
-	w2 = hex_to_value[fp2[i]];
+    w1 = hex_to_value[fp1[i]];
+    w2 = hex_to_value[fp2[i]];
     /* Cumulative bitwise-or to check for non-hex values  */
-	union_w = union_w | (w1|w2);
-	intersect_popcount = intersect_popcount + _popcount[w1 & w2];
+    union_w = union_w | (w1|w2);
+    intersect_popcount = intersect_popcount + _popcount[w1 & w2];
   }
   if (union_w >= BIG) {
-	return -1;
+    return -1;
   }
   return intersect_popcount;
 }
 
 /* Return the Tanitoto between two hex fingerprints, or -1.0 for invalid fingerprints
-   If neither fingerprint has any set bits then return 1.0 instead of inf or NaN */
+   If neither fingerprint has any set bits then return 1.0 */
 /* I spent a lot of time trying out different ways to optimize this code.
    This is quite fast, but feel free to point out better ways! */
 double chemfp_hex_tanimoto(int len, const unsigned char *fp1,
-						   const unsigned char *fp2) {
+                           const unsigned char *fp2) {
   int i=0, union_w=0;
   int union_popcount=0, intersect_popcount=0;
   int w1, w2;
@@ -141,74 +141,74 @@ double chemfp_hex_tanimoto(int len, const unsigned char *fp1,
 
   /* Hex fingerprints really should be even-length since two hex characters
      are used for a single fingerprint byte and all chemfp fingerprints must
-	 be a multiple of 8 bits. I'll allow odd-lengths since I don't see how
+     be a multiple of 8 bits. I'll allow odd-lengths since I don't see how
      that's a bad idea and I can see how some people will be confused by
      expecting odd lengths to work. More specifically, I was confused because
      I used some odd lengths in my tests. ;) */
 
   /* I'll process two characters at a time. Loop-unrolling was about 4% faster. */
   for (; i<upper_bound; i+=2) {
-	w1 = hex_to_value[fp1[i]];
-	w2 = hex_to_value[fp2[i]];
-	w3 = hex_to_value[fp1[i+1]];
-	w4 = hex_to_value[fp2[i+1]];
-	/* Check for illegal characters */
-	union_w |= (w1|w2|w3|w4);
-	/* The largest possible index is w1|w2 = (16 | 15) == 31 and */
+    w1 = hex_to_value[fp1[i]];
+    w2 = hex_to_value[fp2[i]];
+    w3 = hex_to_value[fp1[i+1]];
+    w4 = hex_to_value[fp2[i+1]];
+    /* Check for illegal characters */
+    union_w |= (w1|w2|w3|w4);
+    /* The largest possible index is w1|w2 = (16 | 15) == 31 and */
     /* is only possible when the input is not a legal hex character. */
-	union_popcount += _popcount[w1|w2]+_popcount[w3|w4];
-	/* The largest possible index is w1&w2 = (16 & 16) == 16 */
-	intersect_popcount += _popcount[w1&w2]+_popcount[w3&w4];
+    union_popcount += _popcount[w1|w2]+_popcount[w3|w4];
+    /* The largest possible index is w1&w2 = (16 & 16) == 16 */
+    intersect_popcount += _popcount[w1&w2]+_popcount[w3&w4];
   }
   /* Handle the final byte for the case of odd fingerprint length */
   for (; i<len; i++) {
-	w1 = hex_to_value[fp1[i]];
-	w2 = hex_to_value[fp2[i]];
-	// Check for illegal characters
-	union_w |= (w1|w2);
-	// The largest possible index is (16 | 15) == 31
-	// (and only when the input isn't a legal hex character)
-	union_popcount += _popcount[w1|w2];
-	// The largest possible index is (16 & 16) == 16
-	intersect_popcount += _popcount[w1&w2];
+    w1 = hex_to_value[fp1[i]];
+    w2 = hex_to_value[fp2[i]];
+    // Check for illegal characters
+    union_w |= (w1|w2);
+    // The largest possible index is (16 | 15) == 31
+    // (and only when the input isn't a legal hex character)
+    union_popcount += _popcount[w1|w2];
+    // The largest possible index is (16 & 16) == 16
+    intersect_popcount += _popcount[w1&w2];
   }
   /* Check for illegal character */
   if (union_w >= BIG) {
-	return -1.0;
+    return -1.0;
   }
   /* Special case define that 0/0 = 1.0. This agrees with OpenEye and after
      looking and asking around seems to be the generally accepted choice. */
   if (union_popcount == 0) {
-	return 1.0;
+    return 1.0;
   }
   return (intersect_popcount + 0.0) / union_popcount;  /* +0.0 to coerce to double */
 }
 
 /* Return 1 if the query fingerprint is contained in the target, 0 if it isn't,
-   and -1 if the inputs aren't valid fingerprints */
-/* This is written for the cases that 1) most tests fail and 2) most fingerprints are valid
+   or -1 for invalid fingerprints */
+/* This code assumes that 1) most tests fail and 2) most fingerprints are valid */
 int chemfp_hex_contains(int len, const unsigned char *query_fp,
-						const unsigned char *target_fp) {
+                        const unsigned char *target_fp) {
   int i, query_w, target_w;
   int union_w=0;
   for (i=0; i<len; i++) {
-    /* Subset test is easy; check if query & target == query */
-	/* I'll do word-by-word tests, where the word can also overflow to BIG */
-	/* Do the normal test against BIG to see if there was a non-hex input */
-	query_w = hex_to_value[query_fp[i]];
-	target_w = hex_to_value[target_fp[i]];
-	union_w |= (query_w|target_w);
-	if ((query_w & target_w) != query_w) {
+    /* Subset test is easy; check if query & target == query
+       I'll do word-by-word tests, where the word can also overflow to BIG
+       Do the normal test against BIG to see if there was a non-hex input */
+    query_w = hex_to_value[query_fp[i]];
+    target_w = hex_to_value[target_fp[i]];
+    union_w |= (query_w|target_w);
+    if ((query_w & target_w) != query_w) {
       /* Not a subset, but first, check if there was a a non-hex input */
-	  if (union_w >= BIG) {
-		return -1;
-	  }
-	  return 0;
-	}
+      if (union_w >= BIG) {
+        return -1;
+      }
+      return 0;
+    }
   }
   /* This was a subset, but there might have been a non-hex input */
   if (union_w >= BIG) {
-	return -1;
+    return -1;
   }
   return 1;
 }
@@ -236,46 +236,46 @@ static int byte_popcounts[] = {
 int chemfp_byte_popcount(int len, const unsigned char *fp) {
   int i, popcount = 0;
   for (i=0; i<len; i++) {
-	popcount += byte_popcounts[fp[i]];
+    popcount += byte_popcounts[fp[i]];
   }
   return popcount;
 }
 
 /* Return the population count of the intersection of two byte fingerprints */
 int chemfp_byte_intersect_popcount(int len, const unsigned char *fp1,
-								   const unsigned char *fp2) {
+                                   const unsigned char *fp2) {
   int i, intersect_popcount = 0;
   for (i=0; i<len; i++) {
-	intersect_popcount += byte_popcounts[fp1[i]&fp2[i]];
+    intersect_popcount += byte_popcounts[fp1[i]&fp2[i]];
   }
   return intersect_popcount;
 }
 
 /* Return the Tanitoto between two byte fingerprints, or -1.0 for invalid fingerprints
-   If neither fingerprint has any set bits then return 1.0 instead of inf or NaN */
+   If neither fingerprint has any set bits then return 1.0 */
 double chemfp_byte_tanimoto(int len, const unsigned char *fp1,
-							const unsigned char *fp2) {
+                            const unsigned char *fp2) {
   int i, union_popcount=0, intersect_popcount=0;
   /* Accumulate the total union and intersection popcounts */
   for (i=0; i<len; i++) {
-	union_popcount += byte_popcounts[fp1[i] | fp2[i]];
-	intersect_popcount += byte_popcounts[fp1[i] & fp2[i]];
+    union_popcount += byte_popcounts[fp1[i] | fp2[i]];
+    intersect_popcount += byte_popcounts[fp1[i] & fp2[i]];
   }
   /* Special case for when neither fingerprint has any bytes set */
   if (union_popcount == 0) {
-	return 1.0;
+    return 1.0;
   }
   return (intersect_popcount + 0.0) / union_popcount;  /* +0.0 to coerce to double */
 }
 
 /* Return 1 if the query fingerprint is contained in the target, 0 if it isn't */
 int chemfp_byte_contains(int len, const unsigned char *query_fp,
-						 const unsigned char *target_fp) {
+                         const unsigned char *target_fp) {
   int i;
   for (i=0; i<len; i++) {
-	if ((query_fp[i] & target_fp[i]) != query_fp[i]) {
-	  return 0;
-	}
+    if ((query_fp[i] & target_fp[i]) != query_fp[i]) {
+      return 0;
+    }
   }
   return 1;
 }
