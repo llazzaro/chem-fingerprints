0a0879c984c02095d1dfe1c05085825bbb47761d
dalke <dalke@localhost>
2010-10-13 05:46:07 +0200

    function renaming (all functions in the library will have the chemfp prefix) and removed some old testing code

--- src/heapq.c
+++ src/heapq.c
@@ -1,13 +1,8 @@
-#include "heapq.h"
-
-#ifdef TEST_MAIN
-#include <stdio.h>
-#endif
 
 /* Implement nlargest from the Python */
 
-int heapq_siftdown(int len, void *heap, int startpos, int pos,
-				   heapq_lt lt, heapq_swap swap) {
+int chemfp_heapq_siftdown(int len, void *heap, int startpos, int pos,
+						  chemfp_heapq_lt lt, chemfp_heapq_swap swap) {
   int parentpos, cmp;
   /* Follow the path to the root, moving parents down until finding
 	 a place newitem fits. */
@@ -24,8 +19,8 @@ int heapq_siftdown(int len, void *heap, int startpos, int pos,
   return 0;
 }
 
-int heapq_siftup(int len, void *heap, int pos,
-				 heapq_lt lt, heapq_swap swap) {
+int chemfp_heapq_siftup(int len, void *heap, int pos,
+						chemfp_heapq_lt lt, chemfp_heapq_swap swap) {
   int endpos = len;
   int startpos = pos;
   int cmp, rightpos, childpos;
@@ -49,11 +44,18 @@ int heapq_siftup(int len, void *heap, int pos,
   }
   /* The item at pos contains the original 'pos' item.  Bubble it back up to
 	 its final resting place (by sifting its parents down). */
-  return heapq_siftdown(len, heap, startpos, pos, lt, swap);
+  return chemfp_heapq_siftdown(len, heap, startpos, pos, lt, swap);
 }
 
-int heapq_heapify(int len, void *heap,
-				  heapq_lt lt, heapq_swap swap) {
+#if 0
+
+/* Convert an unsorted heapt into a heap. Used during testing. */
+
+int chemfp_heapq_heapify(int len, void *heap,
+						 chemfp_heapq_lt lt, chemfp_heapq_swap swap);
+
+int chemfp_heapq_heapify(int len, void *heap,
+						 chemfp_heapq_lt lt, chemfp_heapq_swap swap) {
   int i;
   /* Transform bottom-up.  The largest index there's any point to
 	 looking at is the largest with a child index in-range, so must
@@ -63,16 +65,17 @@ int heapq_heapify(int len, void *heap,
 	 and that's again n//2-1.
   */
   for (i=len/2-1; i>=0; i--) {
-	if (heapq_siftup(len, heap, i, lt, swap) == -1)
+	if (chemfp_heapq_siftup(len, heap, i, lt, swap) == -1)
 	  return -1;
   }
   return 0;
 }
+#endif
 
 /* http://en.wikipedia.org/wiki/Heapsort */
 
-int heapq_heapsort(int len, void *heap,
-				   heapq_lt lt, heapq_swap swap) {
+int chemfp_heapq_heapsort(int len, void *heap,
+						  chemfp_heapq_lt lt, chemfp_heapq_swap swap) {
   int end;
   if (len == 0)
 	return 0;
@@ -83,53 +86,3 @@ int heapq_heapsort(int len, void *heap,
   }
   return 0;
 }
-
-#ifdef TEST_MAIN
-
-int int_lt(void *heap, int left, int right) {
-  int *data = (int *) heap;
-  return data[left] < data[right];
-}
-
-void int_swap(void *heap, int left, int right) {
-  int *data = (int *) heap;
-  int tmp = data[left];
-  data[left] = data[right];
-  data[right] = tmp;
-}
-
-void add_int(int len, int *data, int new_value) {
-  if (new_value < data[0]) 
-	return;
-  data[0] = new_value;
-  heapq_siftup(len, data, 0, int_lt, int_swap);
-}
-	
-
-void dump(int len, int *data) {
-  int i;
-  for (i=0; i<len; i++) {
-	printf("%d ", data[i]);
-  }
-  printf("\n");
-}
-
-int main() {
-  int data[] = {3, 9, 2, 4, 5};
-  int new_data[] = {8, 6, 7, 5, 3, 0, 9};
-  int len = sizeof(data)/sizeof(int);
-  int new_len = sizeof(new_data)/sizeof(int);
-  int i;
-
-  heapq_heapify(len, data, int_lt, int_swap);
-  dump(len, data);
-  for (i=0; i<new_len; i++) {
-	add_int(len, data, new_data[i]);
-	dump(len, data);
-  }
-  heapq_heapsort(len, data, int_lt, int_swap);
-  dump(len, data);
-
-  return 0;
-}
-#endif
