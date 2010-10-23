e401fe8c9eb9856297d9236942273a0e7e6aa03f
dalke <dalke@localhost>
2010-10-24 01:19:02 +0200

    was missing the heapq

--- setup.py
+++ setup.py
@@ -12,8 +12,8 @@ setup(name = "chemfp",
       scripts = ["ob2fps", "oe2fps", "rdkit2fps", "sdf2fps"],
 
       ext_modules = [Extension("_chemfp",
-                               ["src/bitops.c", "src/heapq.c",
-                                "src/fps.c",
+                               ["src/bitops.c", "src/chemfp.c",
+                                "src/heapq.c", "src/fps.c",
                                 "src/searches.c",
                                 "src/python_api.c"])],
      )
