f7078dd5e7d7685270c4f21cbc61aa365f967f14
dalke <dalke@localhost>
2010-10-26 13:53:02 +0200

    improved the tests; not as much of a hack now. STill don't like it. Need to find out how it's really supposed to be done

--- tests/test_docstrings.py
+++ tests/test_docstrings.py
@@ -2,15 +2,15 @@ import sys
 import unittest
 import doctest
 
-# I don't know the right way to do this.
-# Faking it for now. It works under unittest but not nosetest.
-class TestEncodings(unittest.TestCase):
-    pass
+# XXX I do not know how to make doctest work with nosetests
+# Even this code here feels like a hack - I want the '-v' support under unittest
 
+class MyLoader(object):
+    def loadTestsFromModule(self, name):
+        return suite()
 
-for x in doctest.DocTestSuite("chemfp.decoders")._tests:
-    name = "test_" + x._dt_test.name.replace(".", "__")
-    setattr(TestEncodings, name, x)
+def suite():
+    return doctest.DocTestSuite("chemfp.decoders")
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest.main(testLoader = MyLoader())
