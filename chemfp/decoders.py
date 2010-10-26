87d79d383fb750985146e1ce10c72465bf3f84a1
dalke <dalke@localhost>
2010-10-26 13:55:06 +0200

    Improved the documentation. Make the bit generation code a bit easier to understand

--- chemfp/decoders.py
+++ chemfp/decoders.py
@@ -1,20 +1,50 @@
+"""chemfp.decoders - decode different fingerprint representations into chemfp form
+
+The chemfp fingerprints are stored as byte strings, with the bytes in
+least-significant bit order (bit #0 is stored in the first/left-most
+byte) and with the bits in most-significant bit order (bit #0 is
+stored in the first/right-most bit of the first byte).
+
+Other systems use different encodings. These include:
+  - the '0 and '1' characters, as in '00111101'
+  - hex encoding, like '3d'
+  - base64 encoding, like 'SGVsbG8h'
+  - CACTVS's variation of base64 encoding
+
+plus variations of different LSB and MSB orders.
+
+This module decodes most of the fingerprint encodings I have come
+across. The fingerprint decoders return a 2-ple of the bit length and
+the chemfp fingerprint. The bit length is None unless the bit length
+is known exactly, which currently is only the case for the binary and
+CACTVS fingerprints. (The hex and other encoders must round the
+fingerprints up to a multiple of 8 bits.)
+
+"""
 import string
 import binascii
 
-_lsb_bit_table = {}
-_msb_bit_table = {}
+_lsb_bit_table = {} # "10000000" -> 1
+_msb_bit_table = {} # "00000001" -> 1
 
 _reverse_bits_in_a_byte_transtable = None
 
+# These are in lsb order; 
+_lsb_4bit_patterns = (
+    "0000", "1000", "0100", "1100",
+    "0010", "1010", "0110", "1110",
+    "0001", "1001", "0101", "1101",
+    "0011", "1011", "0111", "1111")
+
+# Generate '00000000', '10000000', '01000000', ... , '01111111', '11111111'
+def _lsb_8bit_patterns():
+    for right in _lsb_4bit_patterns:
+        for left in _lsb_4bit_patterns:
+            yield left + right
+
 def _init():
-    # This creates the 256 bit patterns:
-    #    00000000, 10000000, 01000000, 11000000 ... 11111111
-    def _C(it):
-        for i in it:
-            yield "0" + i
-            yield "1" + i
     to_trans = [None]*256
-    for value, bit_pattern in enumerate(_C(_C(_C(_C(_C(_C(_C("01")))))))):
+    for value, bit_pattern in enumerate(_lsb_8bit_patterns()):
         # Each pattern maps to the byte
         byte_value = chr(value)
         to_trans[value] = chr(int(bit_pattern, 2))
@@ -60,6 +90,17 @@ assert _msb_bit_table["1000000"] == "\x40"
 
 
 def from_binary_lsb(text):
+    """Convert a string like '00010101' (bit 0 here is off) into '\\xa8'
+
+    The encoding characters '0' and '1' are in LSB order, so bit 0 is the left-most field.
+    The result is a 2-ple of the fingerprint length and the decoded chemfp fingerprint
+
+    >>> from_binary_lsb('00010101')
+    (8, '\\xa8')
+    >>> from_binary_lsb('11101')
+    (5, '\\x17')
+    >>>
+    """
     try:
         N = len(text)
         bytes = []
@@ -70,6 +111,18 @@ def from_binary_lsb(text):
         raise TypeError("Not a binary string")
 
 def from_binary_msb(text):
+    """Convert a string like '10101000' (bit 0 here is off) into '\\xa8'
+
+    The encoding characters '0' and '1' are in MSB order, so bit 0 is the right-most field.
+
+    >>> from_binary_msb('10101000')
+    (8, '\\xa8')
+    >>> from_binary_msb('00010101')
+    (8, '\\x15')
+    >>> from_binary_msb('00111')
+    (5, '\\x07')
+    >>>
+    """
     try:
         N = len(text)
         bytes = []
@@ -86,6 +139,17 @@ def from_binary_msb(text):
 
 
 def from_base64(text):
+    """Decode a base64 encoded fingerprint string
+
+    The encoded fingerprint must be in chemfp form, with the bytes in
+    LSB order and the bits in MSB order.
+
+    >>> from_base64("SGk=")
+    (None, 'Hi')
+    >>> from_base64("SGk=")[1].encode("hex")
+    '4869'
+    >>> 
+    """
     try:
         # This is the same as doing text.decode("base64") but since I
         # need to catch the exception, I might as well work with the
@@ -101,12 +165,42 @@ def from_base64(text):
 #    return (None, text.decode("base64").translate(_reverse_bits_in_a_byte_transtable), None)
     
 def from_hex(text):
+    """Decode a hex encoded fingerprint string
+
+    The encoded fingerprint must be in chemfp form, with the bytes in
+    LSB order and the bits in MSB order.
+
+    >>> from_hex('10f2')
+    (None, '\\x10\\xf2')
+    >>>
+
+    Raises a TypeError if the hex string is not a multiple of 2 bytes long
+    or if it contains a non-hex character.
+    """
     return (None, text.decode("hex"))
 
 def from_hex_msb(text):
+    """Decode a hex encoded fingerprint string where the bits and bytes are in MSB order
+
+    >>> from_hex_msb('10f2')
+    (None, '\\xf2\\x10')
+    >>>
+
+    Raises a TypeError if the hex string is not a multiple of 2 bytes long
+    or if it contains a non-hex character.
+    """
     return (None, text.decode("hex")[::-1])
 
 def from_hex_lsb(text):
+    """Decode a hex encoded fingerprint string where the bits and bytes are in LSB order
+
+    >>> from_hex_lsb('102f')
+    (None, '\\x08\\xf4')
+    >>> 
+
+    Raises a TypeError if the hex string is not a multiple of 2 bytes long
+    or if it contains a non-hex character.
+    """
     return (None, text.decode("hex").translate(_reverse_bits_in_a_byte_transtable))
 
 
@@ -126,16 +220,40 @@ def from_hex_lsb(text):
 # 0111 1000 0100 0000 0000 0101 0000 0000 0000 0000 0000 0000
 
 def from_cactvs(text):
+    """Decode a 881-bit CACTVS-encoded fingerprint used by PubChem
+
+    >>> from_cactvs("AAADceB7sQAEAAAAAAAAAAAAAAAAAWAAAAAwAAAAAAAAAAABwAAAHwIYAAAADA" +
+    ...             "rBniwygJJqAACqAyVyVACSBAAhhwIa+CC4ZtgIYCLB0/CUpAhgmADIyYcAgAAO" +
+    ...             "AAAAAAABAAAAAAAAAAIAAAAAAAAAAA==")
+    (881, '\\x07\\xde\\x8d\\x00 \\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x80\\x06\\x00\\x00\\x00\\x0c\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x80\\x03\\x00\\x00\\xf8@\\x18\\x00\\x00\\x000P\\x83y4L\\x01IV\\x00\\x00U\\xc0\\xa4N*\\x00I \\x00\\x84\\xe1@X\\x1f\\x04\\x1df\\x1b\\x10\\x06D\\x83\\xcb\\x0f)%\\x10\\x06\\x19\\x00\\x13\\x93\\xe1\\x00\\x01\\x00p\\x00\\x00\\x00\\x00\\x00\\x80\\x00\\x00\\x00\\x00\\x00\\x00\\x00@\\x00\\x00\\x00\\x00\\x00\\x00\\x00\\x00')
+    >>>
+
+    For format details, see
+      ftp://ftp.ncbi.nlm.nih.gov/pubchem/specifications/pubchem_fingerprints.txt
+    """
     fp = text.decode("base64")
     # first 4 bytes are the length (struct.unpack(">I"))
-    assert fp[:4] == '\x00\x00\x03q', "not 881 bits?"
+    if fp[:4] != '\x00\x00\x03q':
+        raise TypeError("This implementation is hard-coded for 881 bit CACTVS fingerprints")
     return 881, fp[4:].translate(_reverse_bits_in_a_byte_transtable)
 
 
 def import_decoder(path):
+    """Find a decoder function given its full name, as in 'chemfp.decoders.from_cactvs'
+
+    This function imports any intermediate modules, which may be a security concern.
+    """
     terms = path.split(".")
     if not terms:
         raise TypeError("missing import name")
+    if "" in terms:
+        raise TypeError("Empty module name in {path!r}".format(path=path))
+
+    # It's impossible to tell if the dotted terms corresponds to
+    # module or class/instance attribute lookups, so I don't know
+    # which fields are imports and which fields are getattrs. To get
+    # around that, I'll import everything, and if that fails I'll
+    # remove the deepest term and try again.
     tmp_terms = terms[:]
     while tmp_terms:
         try:
@@ -144,13 +262,16 @@ def import_decoder(path):
             del tmp_terms[-1]
         else:
             break
+    # I've imported as deep as possible.
+    # Now start from the top and work down with getattr calls
     obj = __import__(terms[0], level=0)
     for subattr in terms[1:]:
-       obj = getattr(obj, subattr)
+       obj = getattr(obj, subattr, None)
        if obj is None:
            raise TypeError(("Unable to import a decoder: "
-                           "stopped at {attr!r} in {path!r}").format(
+                            "stopped at {attr!r} in {path!r}").format(
                    attr=subattr, path=path))
+
     return obj
 
 
@@ -163,21 +284,21 @@ def _A(arg, action, decoder, help):
     _decoder_table[arg.lstrip("-").replace("-","_")] = decoder
 
 _A("--binary", "store_true", from_binary_lsb,
-   "encoded using 0s and 1s. #0 is first")
+   "Encoded with the characters '0' and '1'. Bit #0 comes first. Example: 00100000 encodes the value 4")
 _A("--binary-msb", "store_true", from_binary_msb,
-   "encoded using 0s and 1s. #0 is last")
+   "Encoded with the characters '0' and '1'. Bit #0 comes last. Example: 00000100 encodes the value 4")
 _A("--hex", "store_true", from_hex,
-   "hex-encoded bytes. #0 is first bit of first byte")
+   "Hex encoded. Bit #0 is the first bit (1<<0) of the first byte. Example: 01f2 encodes the value \\x01\\xf2 = 498")
 _A("--hex-lsb", "store_true", from_hex_lsb,
-   "hex-encoded bytes. #0 is eigth bit of first byte")
+   "Hex encoded. Bit #0 is the eigth bit (1<<7) of the first byte. Example: 804f encodes the value \\x01\\xf2 = 498")
 _A("--hex-msb", "store_true", from_hex_msb,
-   "hex-encoded bytes. #0 is first bit of last byte")
+   "Hex encoded. Bit #0 is the first bit (1<<0) of the last byte. Example: f201 encodes the value \\x01\\xf2 = 498")
 _A("--base64", "store_true", from_base64,
-   "base64 encoded bytes. #0 is first bit of first byte")
+   "Base-64 encoded. Bit #0 is first bit (1<<0) of first byte. Example: AfI= encodes value \\x01\\xf2 = 498")
 _A("--cactvs", "store_true", from_cactvs,
-   help="CACTVS encoding, based on base64")
+   help="CACTVS encoding, based on base64 and includes a version and bit length")
 _A("--decoder", "store", None,
-    help="import DECODER and use it to decode the fingerprint")
+    help="import and use the DECODER function to decode the fingerprint")
 
 def _add_decoding_group(parser):
     decoding_group = parser.add_argument_group("Fingerprint decoding options")
