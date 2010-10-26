56407c3668e17b1ad4bb27ccf9d303f6478f0ca3
dalke <dalke@localhost>
2010-10-26 13:53:44 +0200

    changed the names

--- chemfp/search.py
+++ chemfp/search.py
@@ -7,7 +7,7 @@ from chemfp import bitops
 # This code is so much simpler than the C extension code.
 # And about 6 times slower, even with bitops.byte_tanimoto in C!
 
-def generic_tanimoto(query, targets, threshold):
+def generic_tanimoto_search(query, targets, threshold):
     for fp, id in targets:
         score = bitops.byte_tanimoto(query, fp)
         if score == -1:
@@ -18,7 +18,7 @@ def generic_tanimoto(query, targets, threshold):
 def generic_tanimoto_count(query, targets, threshold):
     return sum(1 for (fp, id) in targets if bitops.byte_tanimoto(query, fp) >= threshold)
 
-def generic_tanimoto_knearest(query, targets, k, threshold):
+def generic_tanimoto_knearest_search(query, targets, k, threshold):
     results = heapq.nlargest(k, generic_tanimoto(query, targets, threshold),
                              key=operator.itemgetter(1))
     return results
