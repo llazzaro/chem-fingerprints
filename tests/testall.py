781afbfb8e765a88463ec7ecf280f7e1d1d033d9
dalke <dalke@localhost>
2010-11-03 20:02:38 +0100

    updated for the oe and rdkit command-line tests

--- tests/testall.py
+++ tests/testall.py
@@ -9,7 +9,8 @@ import test_docstrings
 suite = unittest.TestSuite()
 suite.addTest(test_docstrings.suite())
 
-for name in ("test_chemfp", "test_sdf_reader", "test_sdf2fps"):
+for name in ("test_chemfp", "test_sdf_reader", "test_sdf2fps", "test_oe2fps",
+             "test_rdkit2fps"):
     m = __import__(name)
     suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(m))
 
