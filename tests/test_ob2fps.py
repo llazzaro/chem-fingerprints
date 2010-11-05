013f57f7c6f7c81d775fed179273552fb5d37481
dalke <dalke@localhost>
2010-11-05 21:46:44 +0100

    added I/O tests

--- tests/test_ob2fps.py
+++ tests/test_ob2fps.py
@@ -1,5 +1,8 @@
-import unittest
+import os
+import shutil
 import sys
+import tempfile
+import unittest
 
 from chemfp.commandline import ob2fps
 
@@ -16,18 +19,46 @@ class TestFingerprintTypes(unittest.TestCase):
     def test_FP2(self):
         headers, fps = run_split("--FP2", 19)
         self.assertEquals(headers["#type"], "OpenBabel-FP2/1")
+        self.assertEquals(fps[0], "200206000000000402800e00040140010100014008206200000200c0082200080200500201c9804100270538000000402000a2040080c1240001c2c2004600200c200c04020800200410a0001490000200a803c018005400c80c00000000810100840000880064a0124010000000080102060142400110200a00000004800000 9425004")
     def test_FP3(self):
         headers, fps = run_split("--FP3", 19)
         self.assertEquals(headers["#type"], "OpenBabel-FP3/1")
+        self.assertEquals(fps[0], "0400000000b001 9425004")
     def test_FP4(self):
         headers, fps = run_split("--FP4", 19)
         self.assertEquals(headers["#type"], "OpenBabel-FP4/1")
+        self.assertEquals(fps[0], "1100000000000000000080000000000000010000000c9800000000000000000000000640407800 9425004")
     def test_MACCS(self):
         headers, fps = run_split("--MACCS", 19)
         self.assertEquals(headers["#type"], "OpenBabel-MACCS/1")
+        self.assertEquals(fps[0], "800400000002080019cc40eacdec980baea378ef1b 9425004")
 
 class TestIO(unittest.TestCase):
-    pass
+    def test_compressed_auto(self):
+        header, fps = run_split("--FP3", 19, support.PUBCHEM_SDF_GZ)
+        self.assertEquals(fps[0], "0400000000b001 9425004")
+    def test_compressed_specified(self):
+        header, fps = run_split("--FP3 --in sdf.gz", 19, support.PUBCHEM_SDF_GZ)
+        self.assertEquals(fps[0], "0400000000b001 9425004")
+    def test_format_specified(self):
+        header, fps = run_split("--FP3 --in sdf", 19, support.PUBCHEM_ANOTHER_EXT)
+        self.assertEquals(fps[0], "0400000000b001 9425004")
+    def test_output(self):
+        dirname = tempfile.mkdtemp(prefix="test_ob2fps")
+        output_filename = os.path.join(dirname, "blah.fps")
+        assert len(output_filename.split()) == 1 # ensure no whitespace
+        try:
+            output = runner.run("--FP3 -o " + output_filename)
+            assert len(output) == 0
+            with open(output_filename) as f:
+                result = f.readlines()
+        finally:
+            shutil.rmtree(dirname)
+        self.assertEquals(result[0], "#FPS1\n")
+        fps = [line for line in result if not line.startswith("#")]
+        self.assertEquals(len(fps), 19)
+        self.assertEquals(fps[0], "0400000000b001 9425004\n")
+        
 
 class TestMACCS(unittest.TestCase):
     def test_bitorder(self):
