dd7b47daf2428a178d925eaa99d216f2cebe386f
dalke <dalke@localhost>
2010-11-05 21:49:27 +0100

    added openbabel tests

--- tests/testall.py
+++ tests/testall.py
@@ -10,7 +10,7 @@ suite = unittest.TestSuite()
 suite.addTest(test_docstrings.suite())
 
 for name in ("test_chemfp", "test_sdf_reader", "test_sdf2fps", "test_oe2fps",
-             "test_rdkit2fps", "test_rdkit"):
+             "test_rdkit2fps", "test_rdkit", "test_ob2fps"):
     m = __import__(name)
     suite.addTest(unittest.defaultTestLoader.loadTestsFromModule(m))
 
