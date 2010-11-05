fd20f2bdd6da74ecd77b9f2824d19b50ccee0aba
dalke <dalke@localhost>
2010-11-05 18:37:24 +0100

    got the test to work

--- tests/test_ob2fps.py
+++ tests/test_ob2fps.py
@@ -3,37 +3,18 @@ import sys
 
 from chemfp.commandline import ob2fps
 
+import support
+
 real_stdout = sys.stdout
 
-PUBCHEM_SDF = "pubchem.sdf"
-
-def run(s):
-    args = s.split()
-    try:
-        sys.stdin = open(PUBCHEM_SDF)
-        sys.stdout = stdout = SIO()
-        sdf2fps.main(args)
-    finally:
-        sys.stdout = real_stdout
-        sys.stdin = real_stdin
-    result = stdout.getvalue().splitlines()
-    if result:
-        assert result[0] == "#FPS1"
-    return result
-
-def run_fps(s, expect_length=None):
-    result = run(s)
-    while result[0].startswith("#"):
-        del result[0]
-    if expect_length is not None:
-        assert len(result) == expect_length
-    return result
-
+runner = support.Runner(ob2fps.main)
+run = runner.run
+run_fps = runner.run_fps
 
 class TestFingerprintTypes(unittest.TestCase):
     def test_FP2(self):
-        result = run("--FP2")
-        self.assertEquals(len(result), 11)
+        result = run_fps("--FP2")
+        self.assertEquals(len(result), 19)
 
 if __name__ == "__main__":
     unittest.main()
