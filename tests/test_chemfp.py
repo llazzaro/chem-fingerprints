6b355a633664f06613d76b617731fe23d07ef05f
dalke <dalke@localhost>
2010-12-13 22:40:46 +0100

    using unittest2

--- tests/test_chemfp.py
+++ tests/test_chemfp.py
@@ -1,4 +1,4 @@
-import unittest
+import unittest2
 import re
 
 import chemfp
@@ -6,10 +6,10 @@ import _chemfp
 
 version_pattern = re.compile(r"\d+\.\d+(\.\d)?((a|b|pre)\d+)?$")
 
-class SystemTests(unittest.TestCase):
+class SystemTests(unittest2.TestCase):
     def test_version(self):
         m = version_pattern.match(_chemfp.version())
         self.assertNotEqual(m, None, "bad version: %s" % (_chemfp.version(),))
 
 if __name__ == "__main__":
-    unittest.main()
+    unittest2.main()
