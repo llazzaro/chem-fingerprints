9b28b77aa39845b93c446f5729d233d598badac2
dalke <dalke@localhost>
2010-12-13 22:40:03 +0100

    migrated to unittest2

--- tests/test_docstrings.py
+++ tests/test_docstrings.py
@@ -1,16 +1,17 @@
 import sys
-import unittest
+import unittest2
 import doctest
 
 # XXX I do not know how to make doctest work with nosetests
 # Even this code here feels like a hack - I want the '-v' support under unittest
 
-class MyLoader(object):
-    def loadTestsFromModule(self, name):
-        return suite()
+#class MyLoader(object):
+#    def loadTestsFromModule(self, name):
+#        return suite()
 
-def suite():
-    return doctest.DocTestSuite("chemfp.decoders")
+def load_tests(loader, tests, ignore):
+    tests.addTests(doctest.DocTestSuite("chemfp.decoders"))
+    return tests
 
 if __name__ == "__main__":
-    unittest.main(testLoader = MyLoader())
+    unittest2.main()
