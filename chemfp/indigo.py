e5078ce5853189514ff4153cb12a6f41932219f5
dalke <dalke@localhost>
2011-06-03 23:56:41 +0200

    extract just the relevant parts of the Indigo fingerprints

--- chemfp/indigo.py
+++ chemfp/indigo.py
@@ -12,20 +12,39 @@ _indigo = Indigo()
 
 SOFTWARE = "Indigo/" + _indigo.version()
 
+# There is no way to get the sizes from the SDK API.  While it is
+# possible to change the fingerprint sizes, I decided to not support
+# that for now since it appears that things will change.
+
+# All fingerprints are currently stored in a single mega-fingerprint
+# object, broken down into parts:
+#   3   bytes for "extra" bits
+#  25*8 bytes for "ordinary" part
+#   8*8 bytes for "similarity" part
+#  10*8 bytes for "tautomer" part
+#  15*8 bytes for "resonance" part
+_ORD = 3
+_SIM = 3+(25)*8
+_TAU = 3+(25+8)*8
+_RES = 3+(25+8+10)*8
+_END = 3+(25+8+10+15)*8
+
 _expected_sizes = {
-    "sim": 3736,
-    "sub": 3736,
-    "sub-res": 3736,
-    "sub-tau": 3736,
-    "full": 3736,
+    "sim": (_TAU-_SIM)*8,
+    "sub": ((_SIM-_ORD)+(_END-_RES))*8,
+    "sub-res": (_END-_RES)*8,
+    "sub-tau": (_RES-_TAU)*8,
+    "full": (_END-_ORD)*8,
     }
 
-def _initialize_sizes():
-    mol = _indigo.loadMolecule("c1ccccc1O")
+def _verify():
+    mol = _indigo.loadMolecule("c1ccccc1OSNOP")
+    fp = mol.fingerprint("sim")
+    
     for name, expected_num_bits in _expected_sizes.items():
         num_bits = len(mol.fingerprint(name).toString())*4
         assert num_bits == expected_num_bits, (name, expected_num_bits, num_bits)
-_initialize_sizes()
+
 
 class UnknownFormat(KeyError):
     def __str__(self):
@@ -89,18 +108,12 @@ class _IndigoFingerprinter(types.Fingerprinter):
         return mol.fingerprint(self.indigo_name)
 
     def _get_reader(self, source, format, kwargs):
-        indigo_name = self.indigo_name
         assert not kwargs, "need to support kwargs here"
-
         reader = read_structures(source, format)
-        
-        def read_indigo_fingerprints(indigo_name, reader):
-            # XXX does read_structures need to take the Indigo
-            # instance with the correct fingerprint sizes?
-            for (title, mol) in reader:
-                # toBuffer returns an array.array but I want a byte string
-                yield mol.fingerprint(indigo_name).toBuffer().tostring(), title
-        return read_indigo_fingerprints(indigo_name, reader)
+        #for fp, title in self._read_indigo_fingerprints(self.indigo_name, reader):
+        #    assert len(fp)*8 == self.num_bits, (len(fp)*8, self.num_bits)
+        #    yield fp, title
+        return  self._read_indigo_fingerprints(self.indigo_name, reader)
 
 # TODO: These can all be parameterized. Each one needs its own Indigo()
 #   with the appropriate setOption()
@@ -110,24 +123,68 @@ class IndigoSimilarityFingerprinter_v1(_IndigoFingerprinter):
     name = "Indigo-Similarity/1"
     software = SOFTWARE
     indigo_name = "sim"
+
+    def _read_indigo_fingerprints(self, indigo_name, reader):
+        START = _SIM
+        END = _TAU
+        for (title, mol) in reader:
+            fp = mol.fingerprint(indigo_name).toBuffer().tostring()
+            #assert len(set(fp[3:START])) == 1, fp.encode("hex")
+            #assert len(set(fp[END:])) == 1, fp.encode("hex")
+            yield fp[START:END], title
                               
 class IndigoSubstructureFingerprinter_v1(_IndigoFingerprinter):
     name = "Indigo-Substructure/1"
     software = SOFTWARE
     indigo_name = "sub"
+
+    def _read_indigo_fingerprints(self, indigo_name, reader):
+        START1 = _ORD
+        END1 = _SIM
+        START2 = _RES
+        END2 = _END
+        for (title, mol) in reader:
+            fp = mol.fingerprint(indigo_name).toBuffer().tostring()
+            #assert len(set(fp[END1:START2])) == 1, fp.encode("hex")
+            yield fp[START1:END1]+fp[START2:END2], title
+
                               
 class IndigoResonanceSubstructureFingerprinter_v1(_IndigoFingerprinter):
     name = "Indigo-ResonanceSubstructure/1"
     software = SOFTWARE
     indigo_name = "sub-res"
+
+    def _read_indigo_fingerprints(self, indigo_name, reader):
+        START = _RES
+        END = _END
+        for (title, mol) in reader:
+            fp = mol.fingerprint(indigo_name).toBuffer().tostring()
+            #assert len(set(fp[3:START])) == 1, fp.encode("hex")
+            yield fp[START:END], title
+
                               
 class IndigoTautomerSubstructureFingerprinter_v1(_IndigoFingerprinter):
     name = "Indigo-TautomerSubstructure/1"
     software = SOFTWARE
     indigo_name = "sub-tau"
+
+    def _read_indigo_fingerprints(self, indigo_name, reader):
+        START = _TAU
+        END = _RES
+        for (title, mol) in reader:
+            fp = mol.fingerprint(indigo_name).toBuffer().tostring()
+            #assert len(set(fp[3:START])) == 1, fp.encode("hex")
+            #assert len(set(fp[END:])) == 1, fp.encode("hex")
+            yield fp[START:END], title
                               
 class IndigoFullFingerprinter_v1(_IndigoFingerprinter):
     name = "Indigo-Full/1"
     software = SOFTWARE
     indigo_name = "full"
                               
+    def _read_indigo_fingerprints(self, indigo_name, reader):
+        START = _ORD
+        END = _END
+        for (title, mol) in reader:
+            fp = mol.fingerprint(indigo_name).toBuffer().tostring()
+            yield fp[3:], title
