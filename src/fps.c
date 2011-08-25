ab20d2293fb30c0817765afa9b6d85658389ba15
dalke <dalke@localhost>
2011-08-25 21:42:39 +0200

    Changed the code to only allow tab as the delimiter character

--- src/fps.c
+++ src/fps.c
@@ -29,20 +29,17 @@ static int parse_line(
     return CHEMFP_UNEXPECTED_FINGERPRINT_LENGTH;
 
   s = line+fp_field_len;
-  /* The only legal thing here is a space or a tab. */
-  /* There might be some other character, including a NUL */
-  /* XXX Why do I allow multiple whitespace ? Check the spec! */
-  ws_len = strspn(s, " \t");  // \v? \f? \r?
-  if (ws_len == 0) {
-    switch (s[0]) {
-    case '\n': return CHEMFP_MISSING_ID;
-    case '\r': if (s[1] == '\n') return CHEMFP_MISSING_ID; /* else fallthrough */
-    case '\v':
-    case '\f': return CHEMFP_UNSUPPORTED_WHITESPACE;
-    default: return CHEMFP_BAD_FINGERPRINT;
-    }
+  /* The only legal thing here is a tab. */
+  /* Check if it's some other character, including a NUL */
+  switch (s[0]) {
+  case '\n': return CHEMFP_MISSING_ID;
+  case '\r': if (s[1] == '\n') return CHEMFP_MISSING_ID; /* else fallthrough */
+  case ' ':
+  case '\v':
+  case '\f': return CHEMFP_UNSUPPORTED_WHITESPACE;
+  default: return CHEMFP_BAD_FINGERPRINT;
   }
-  s += ws_len;
+  s++;
 
   /* You must pass in a newline-terminated string to this function.
      Therefore, this function will finish while inside the string.
@@ -50,6 +47,7 @@ static int parse_line(
   tmp_id_len = strcspn(s, " \t\n\v\f\r");
   switch (s[tmp_id_len]) {
   case '\0': return CHEMFP_BAD_ID;
+  case ' ':
   case '\v':
   case '\f': return CHEMFP_UNSUPPORTED_WHITESPACE;
   case '\r': if (s[tmp_id_len+1] != '\n') return CHEMFP_UNSUPPORTED_WHITESPACE;
