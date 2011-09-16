b8e45c5dfc7772b9d318abed0154efadb7aa0288
dalke <dalke@localhost>
2011-09-16 16:13:10 +0100

    Added a newline so the erorr message is on its own line

--- chemfp/commandline/oe2fps.py
+++ chemfp/commandline/oe2fps.py
@@ -167,7 +167,7 @@ def main(args=None):
     try:
         io.write_fps1_output(reader, args.output, metadata)
     except ParseError, err:
-        sys.stderr.write("ERROR: %s. Exiting." % (err,))
+        sys.stderr.write("ERROR: %s. Exiting.\n" % (err,))
         raise SystemExit(1)
     
 if __name__ == "__main__":
