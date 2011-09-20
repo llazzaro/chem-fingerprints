378b09f4e72bfb5837ca282dc7a50fbf76f49af8
dalke <dalke@localhost>
2011-09-20 22:18:25 +0100

    More fields so I can register nicely with PyPI

--- setup.py
+++ setup.py
@@ -2,12 +2,32 @@
 
 from distutils.core import setup, Extension
 
+DESCRIPTION  = """\
+chemfp is a set of command-lines tools for generating cheminformatics
+fingerprints and searching those fingerprints by Tanimoto similarity,
+as well as a Python library which can be used to build new tools.
+
+These algorithms are designed for the dense, 100-10,000 bit
+fingerprints which occur in small-molecule/pharmaceutical
+chemisty. The core algorithms are implemented in C for performance.
+"""
+
 setup(name = "chemfp",
       version = "1.0",
-      description = "Cheminformatics fingerprint tools",
+      summary = "Cheminformatics fingerprint command-line tools and Python library",
+      description = DESCRIPTION,
       author = "Andrew Dalke",
       author_email = 'dalke@dalkescientific.com',
       url = "http://code.google.com/p/chem-fingerprints/",
+      license = "MIT",
+      classifiers = ["Development Status :: 5 - Production",
+                     "Environment :: Console",
+                     "License :: OSI Approved :: MIT License",
+                     "Operating System :: OS Independent",
+                     "Programming Language :: Python",
+                     "Topic :: Scientific/Engineering :: Chemistry",
+                     "Topic :: Software Development :: Libraries :: Python Modules"],
+      
       packages = ["chemfp", "chemfp.commandline"],
       package_data = {"chemfp": ["rdmaccs.patterns", "substruct.patterns"]},
       scripts = ["ob2fps", "oe2fps", "rdkit2fps", "sdf2fps", "simsearch"],
@@ -20,4 +40,3 @@ setup(name = "chemfp",
                                extra_compile_args = ["-O3"],
                                )],
      )
-
