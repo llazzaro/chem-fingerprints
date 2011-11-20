682f89ccecaf2697ca57f538b9c4787b70b980df
dalke <dalke@localhost>
2011-11-20 23:52:04 +0100

    Add compile-time configuration flag for OpenMP support

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 from distutils.core import setup, Extension
+from distutils.command.build_ext import build_ext
 
 DESCRIPTION  = """\
 chemfp is a set of command-lines tools for generating cheminformatics
@@ -18,11 +19,65 @@ toolkit. chemfp supports the Python libraries from Open Babel,
 OpenEye, and RDKit toolkits.
 """
 
-import sys
-if sys.platform[:3] != "win":
-    extra_compile_args = ["-O3"]
-else:
-    extra_compile_args = []
+USE_OPENMP = False
+
+# chemfp has experimental support for OpenMP, but only for the counts.
+# Preliminary tests 
+def OMP(*args):
+    if USE_OPENMP:
+        return list(args)
+    return []
+
+
+# Compiler-specific configuration settings due to:
+#  http://stackoverflow.com/questions/724664/python-distutils-how-to-get-a-compiler-that-is-going-to-be-used/5192738#5192738
+copt =  {
+    "msvc": OMP("/openmp") + ["/Ox"],
+    "mingw32" : OMP("-fopenmp") + ["-O3", "-ffast-math", "-march=native"],
+
+    "gcc-4.1": ["-O3"], # Doesn't support OpenMP
+    "gcc": OMP("-fopenmp") + ["-O3"],
+    }
+
+lopt =  {
+    "mingw32" : OMP("-fopenmp"),
+
+    "gcc-4.1": ["-O3"], # Doesn't support OpenMP
+    "gcc": OMP("-fopenmp") + ["-O3"],
+    }
+
+
+def _is_gcc(compiler):
+    return "gcc" in compiler or "g++" in compiler
+
+class build_ext_subclass( build_ext ):
+    def build_extensions(self):
+        c = self.compiler.compiler_type
+        if c == "unix":
+            c = self.compiler.compiler[0]
+            if _is_gcc(c):
+                names = [c, "gcc"]
+            else:
+                names = [c]
+        else:
+            names = [c]
+
+        for c in names:
+            if copt.has_key(c):
+                for e in self.extensions:
+                    e.extra_compile_args = copt[ c ]
+                break
+
+        for c in names:
+            if lopt.has_key(c):
+                for e in self.extensions:
+                    e.extra_link_args = lopt[ c ]
+                break
+        
+        build_ext.build_extensions(self)
+
+
+
 
 setup(name = "chemfp",
       version = "1.1a3",
@@ -50,6 +105,6 @@ setup(name = "chemfp",
                                 "src/select_popcount.c",
                                 "src/lauradoux.c", "src/lut16.c", "src/gillies.c",
                                 "src/python_api.c"],
-                               extra_compile_args = extra_compile_args,
                                )],
+      cmdclass = {"build_ext": build_ext_subclass},
      )
